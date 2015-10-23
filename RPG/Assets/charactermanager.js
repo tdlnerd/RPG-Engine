26d6c3887403dff03c3c4c7e8b09b8eeb2f319a4
Tanner <tanner.luce17@beltonnewtech.org>
2015-10-22 19:03:27 -0500

    Minor

--- RPG/Assets/charactermanager.js
+++ RPG/Assets/charactermanager.js
@@ -1,7 +1,23 @@
-﻿function Awake () {
+﻿var damage : UI.Slider;
+function Awake () {
 DontDestroyOnLoad (gameObject);
 	if (FindObjectsOfType(GetType()).Length > 1)
          {
              Destroy(gameObject);
          }
          }
+         
+function OnCollisionEnter2D (hit: Collision2D) {
+	if (hit.gameObject.tag == "Enemy") {
+	damage.value -= hit.gameObject.GetComponent(EnemyDamage).Dam;
+}
+}
+
+function Update () {
+	if (Application.loadedLevelName == "Test") {
+	gameObject.isStatic = true;
+	}
+	else {
+	gameObject.SetActive(true);
+	}
+}
