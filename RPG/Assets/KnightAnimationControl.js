787e3b78ea2123fbaf444622dd478c5fdafa5423
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-10-26 10:44:58 -0500

    Player sprite and walk cycle

---
+++ RPG/Assets/KnightAnimationControl.js
@@ -0,0 +1,21 @@
+﻿var previous: Vector3;
+var velocity: float;
+var Anim : Animator;
+function Update () {
+velocity = ((transform.position.x - previous.x)) / Time.deltaTime;
+Adjust();
+Anim.SetFloat("Speed", velocity);
+}
+
+function Adjust () {
+yield WaitForSeconds (Time.deltaTime);
+previous = transform.position;
+}
+
+function OnCollisionEnter2D(hit : Collision2D) {
+Anim.SetBool("Grounded", true);
+}
+
+function OnCollisionExit2D (hit : Collision2D) {
+Anim.SetBool("Grounded", false);
+}
