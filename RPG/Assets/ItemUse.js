50b7e2910bcb8e07bf2121e28426fe2caa8c4c23
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-10-29 10:45:32 -0500

    Bomb items

---
+++ RPG/Assets/ItemUse.js
@@ -0,0 +1,10 @@
+﻿var BombMenu : UI.Toggle;
+var Bomb : GameObject;
+
+function Update () {
+	if (Input.GetKeyDown("x")) {
+		if (BombMenu.isOn == true) {
+		var b = Instantiate(Bomb, transform.position, transform.rotation);
+	}
+}
+}
