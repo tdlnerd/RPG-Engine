794863ff10d2dcd2226e88e5353662a38cc1cb94
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-10-30 11:27:26 -0500

    Functional UI, BombBox, detailed explosions, hole in wall.

---
+++ RPG/Assets/Bombbox.js
@@ -0,0 +1,21 @@
+﻿var Bomb : float;
+var MessageBox : UI.Text;
+var Collected = false;
+
+function Start () {
+MessageBox = GameObject.Find("MainTextBox").GetComponent(UI.Text);
+}
+
+function OnCollisionStay2D (hit : Collision2D) {
+	Debug.Log("Hit chest " + hit.gameObject.tag);
+	if (hit.gameObject.tag == "Player" && Input.GetKeyDown("up") && Collected == false) {
+	Collected = true;
+	var Man = hit.gameObject.GetComponent(ItemUse);
+	Man.BombCount += Bomb;
+	MessageBox.text = "Acquired " + Bomb + " bombs!";
+	yield WaitForSeconds (1.5);
+	MessageBox.text = " ";
+	gameObject.SetActive(false);
+	}
+
+}
--- RPG/Assets/CanExplode.js
+++ RPG/Assets/CanExplode.js
@@ -1,5 +1,7 @@
-﻿function OnTriggerEnter2D (hit : Collider2D) {
+﻿var Replacement : GameObject;
+function OnTriggerEnter2D (hit : Collider2D) {
 	if (hit.gameObject.tag == "Explode") {
+	var r = Instantiate(Replacement, transform.position, transform.rotation);
 	Destroy(gameObject);
 	}
 }
