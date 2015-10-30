ba24a5159b31a2d0c1b252874b8c7f2c17befd33
Tanner <tanner.luce17@beltonnewtech.org>
2015-10-30 09:55:50 -0500

    Functional Bomb, explodes. Item counter added

--- RPG/Assets/ItemUse.js
+++ RPG/Assets/ItemUse.js
@@ -1,10 +1,13 @@
 ﻿var BombMenu : UI.Toggle;
+var BombCounter : UI.Text;
 var Bomb : GameObject;
-
+var BombCount : int;
 function Update () {
 	if (Input.GetKeyDown("x")) {
-		if (BombMenu.isOn == true) {
+		if (BombMenu.isOn == true && BombCount > 0) {
 		var b = Instantiate(Bomb, transform.position, transform.rotation);
+		BombCount -= 1;
+		}
 	}
-}
+	BombCounter.text = "x " + BombCount.ToString();
 }
