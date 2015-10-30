43870d034b64b9a402290eccb99b1e6d6721135d
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-10-30 09:05:28 -0500

    Bomb is functional

--- RPG/Assets/Bomb.js
+++ RPG/Assets/Bomb.js
@@ -1,5 +1,6 @@
-﻿
+﻿var Smoke : GameObject;
 function Start () {
 yield WaitForSeconds(3);
+var b = Instantiate(Smoke, transform.position, transform.rotation);
 Destroy(gameObject);
 }
---
+++ RPG/Assets/CanExplode.js
@@ -0,0 +1,5 @@
+﻿function OnTriggerEnter2D (hit : Collider2D) {
+	if (hit.gameObject.tag == "Explode") {
+	Destroy(gameObject);
+	}
+}
