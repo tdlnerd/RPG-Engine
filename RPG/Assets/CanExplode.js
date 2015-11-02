c210fd0a039b26d181dbc311bdf36306f552d181
Luce Tanner <tanner.luce17@beltonnewtech.org>
2015-11-02 10:43:37 -0600

    Fixed bomb glitch, started new room.

--- RPG/Assets/CanExplode.js
+++ RPG/Assets/CanExplode.js
@@ -1,16 +1,10 @@
 ﻿var Replacement : GameObject;
 var GOTO : Vector2;
-var Replace = true;
-var DoorScript = true;
 function OnTriggerEnter2D (hit : Collider2D) {
 	if (hit.gameObject.tag == "Explode") {
-		if (Replace == true) {
 	var re = Instantiate(Replacement, transform.position, transform.rotation);
-		if (DoorScript == true) {
 	re.gameObject.GetComponent(Door).Target = GOTO;
-	}
-	}
-	gameObject.SetActive(false);
+	Destroy(gameObject);
 	}
 }
 
