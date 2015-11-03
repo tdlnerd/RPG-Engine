bc0423c571676012fedf0f841e5160cf55c7a15f
Tanner <tanner.luce17@beltonnewtech.org>
2015-11-03 10:00:11 -0600

    Spinning Platform

---
+++ RPG/Assets/TriggerOpen.js
@@ -0,0 +1,9 @@
+﻿var Anim : Animator;
+
+function OnCollisionEnter2D (hit : Collision2D) {
+	Debug.Log("Collision");
+	if (hit.gameObject.tag == "Player") {
+	Debug.Log(Anim.GetBool("Switch"));
+	Anim.SetBool("Switch", true);
+	}
+}
